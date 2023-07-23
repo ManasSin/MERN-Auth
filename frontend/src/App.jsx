84df70ae67d812026b5c2670c856c51b05556191
ManasSin <maltisadan12@gmail.com>
2023-07-23 21:44:33 +0530

    fronend completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="bg-white">
       <Header />
+      <ToastContainer />
       <Outlet />
     </div>
   );
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./App.jsx";
 import "./index.css";
 import {
@@ -11,6 +12,10 @@ import {
 import HomePage from "./components/HomePage.jsx";
 import SignupFrom from "./components/SignupFrom.jsx";
 import LoginFrom from "./components/LoginFrom.jsx";
+import store from "./store/store.js";
+import Logout from "./components/Logout.js";
+import PrivateRoute from "./components/PrivateRoute.jsx";
+import ProfileScreen from "./components/ProfileScreen.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -18,12 +23,18 @@ const router = createBrowserRouter(
       <Route index={true} path="/" element={<HomePage />} />
       <Route path="/register" element={<SignupFrom />} />
       <Route path="/login" element={<LoginFrom />} />
+      <Route path="/logout" element={<Logout />} />
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/profile" element={<ProfileScreen />} />
+      </Route>
     </Route>
   )
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
 );
